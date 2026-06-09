a93564a933f243b7c11498dd5553e5023a2d934f
avikeshari <keshari.avinash7@gmail.com>
2026-06-09 18:58:28 +0530

    Separate Dislike using Redux

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { isSortable } from '@dnd-kit/react/sortable';*/
 //import { Reactions } from './components/Reactions';
 //import { ReactionsContext } from './contexts/ReactionsStore.jsx';
 import { useDispatch , useSelector } from 'react-redux';
-import { selectLikes, setLikes ,setDislikes } from './redux/features/reactionSlice.js';
+import { selectLikes, selectDislikes , setLikes ,setDislikes } from './redux/features/reactionSlice.js';
 
 /*const App = () => {
 
@@ -71,9 +71,10 @@ import { selectLikes, setLikes ,setDislikes } from './redux/features/reactionSli
 
 const App = () => {
     const likes = useSelector(selectLikes);
+    const dislikes = useSelector(selectDislikes);
     const dispatch = useDispatch();
     return <div>
-        <h1>Likes: { likes }</h1>
+        <h1>Likes: { likes }, Dislikes: { dislikes }</h1>
         <button onClick={() => dispatch(setLikes())}>Like</button>
         &nbsp;
         <button onClick={() => dispatch(setDislikes())}>Dislike</button>
--- src/redux/features/reactionSlice.js
+++ src/redux/features/reactionSlice.js
@@ -2,16 +2,19 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export const reactionSlice = createSlice({
     name: 'reaction',
-    initialState: { likes: 0 },
+    initialState: { likes: 0 , dislikes: 0 },
     reducers: {
         setLikes: (state,action) => {
             state.likes += 1;
         },
-        setDislikes: (state,action) => {
+        /*setDislikes: (state,action) => {
             if (state.likes > 0){
                 state.likes -= 1;
             }
             else;
+        }*/
+       setDislikes: (state,action) => {
+            state.dislikes += 1;
         }
     }
 });
@@ -19,6 +22,6 @@ export const reactionSlice = createSlice({
 export const { setLikes, setDislikes } = reactionSlice.actions;
 
 export const selectLikes = (state) => state.reaction.likes; //Exporting selector to get the likes from the state
-//export const selectDislikes = (state) => state.reaction.dislikes;
+export const selectDislikes = (state) => state.reaction.dislikes;
 
 export default reactionSlice.reducer;
